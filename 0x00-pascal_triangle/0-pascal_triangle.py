c6116ebc6f03f920e57e95b885b49451368828dd
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-27 18:11:40 +0300

    Adding Pascal"s Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,19 +3,13 @@
 
 def pascal_triangle(n):
     """ Pascal Triangle Solution using Python """
-    if n <= 0:
-        return []
-    
     triangle =[]
 
-    for i in range(n):
-        if i == 0:
-            triangle = [1]
-        else:
-            row = [1]
+    if n > 0:
+        for i in range(n):
+            row = [1] * (i + 1)
             for j in range(1, i):
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-            row.append(1)
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
             triangle.append(row)
 
     return triangle
