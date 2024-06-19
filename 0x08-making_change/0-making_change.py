6f8061020849a8076e5801c1e1d6c096f61d6d88
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-06-19 23:47:09 +0300

    Adding Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,11 +7,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     
-    coins = coins.sort(reverse=True)
+    coins.sort(reverse=True)
     count = 0
 
     for i in coins:
-        if total > i and total > 0:
+        while total >= i:
+            if total == 0:
+                break
             total -= i
             count += 1
 
